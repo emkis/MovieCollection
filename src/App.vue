30949c1cf30084dc6d2c42886498363d54a8bfe7
emkis <nicolasemkis@gmail.com>
2021-06-03 21:22:03 -0300

    refactor: remove fixed component within app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <Movies />
+  <RouterView />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { WakeUpApi } from '@/services/api/wake-up-api'
-import { Movies } from '@/pages/Movies'
 
 export default defineComponent({
   name: 'App',
-  components: { Movies },
-  setup() {
-    WakeUpApi.init()
-  },
 })
 </script>
