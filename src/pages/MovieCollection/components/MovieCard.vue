c984b5a7465ee3a382e8fa17aa8a27f9fa49a9ac
emkis <nicolasemkis@gmail.com>
2021-06-06 03:05:09 -0300

    feat: add loader variant

--- src/pages/MovieCollection/components/MovieCard.vue
+++ src/pages/MovieCollection/components/MovieCard.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="MovieCard">
-    <Heading level="3">{{ movie.name }}</Heading>
+    <div class="MovieCard__content">
+      <Heading level="3">{{ movie.name }}</Heading>
 
-    <div class="MovieCard__details">
-      <Text as="span">{{ movie.year }}</Text>
-      <Text as="span"> • </Text>
-      <Text as="span">{{ categories }}</Text>
+      <div class="MovieCard__details">
+        <Text as="span">{{ movie.year }}</Text>
+        <Text as="span"> • </Text>
+        <Text as="span">{{ categories }}</Text>
+      </div>
+
+      <div class="MovieCard__rating">
+        <Component :is="RatingIcon" size="30" :color="EThemeColors.geraldine" />
+        <Text as="strong">{{ movie.score }}%</Text>
+      </div>
     </div>
 
-    <div class="MovieCard__rating">
-      <Component :is="RatingIcon" size="30" :color="EThemeColors.geraldine" />
-      <Text as="strong">{{ movie.score }}%</Text>
-    </div>
+    <Loader v-if="loading" />
   </div>
 </template>
 
@@ -24,13 +28,15 @@ import { EThemeColors } from '@/services/theme'
 import { Heading } from '@/components/Heading'
 import { Text } from '@/components/Text'
 import { IconHeart, IconHalfHeart } from '@/components/Icons'
+import { Loader } from '@/components/Loader'
 
 export default defineComponent({
   name: 'MovieCard',
   props: {
+    loading: { type: Boolean, default: false },
     movie: { type: Object as PropType<MovieCard>, required: true },
   },
-  components: { Heading, Text },
+  components: { Heading, Text, Loader },
   setup(props) {
     const categories = computed(() => props.movie.category.join(', '))
     const RatingIcon = computed(() => (props.movie.score < 55 ? IconHalfHeart : IconHeart))
@@ -42,8 +48,10 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .MovieCard {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding: rem(24px);
-  text-align: left;
   background: var(--concept-over-background);
   border-radius: $border-radius-m;
   transition: transform 100ms ease, box-shadow 100ms ease;
@@ -59,8 +67,18 @@ export default defineComponent({
     transform: scale(0.98);
   }
 
-  > * + * {
-    margin-top: rem(6px);
+  &__content {
+    text-align: left;
+
+    > * + * {
+      margin-top: rem(6px);
+    }
+  }
+
+  &__details {
+    :nth-child(2) {
+      margin: 0 3px;
+    }
   }
 
   &__rating {
