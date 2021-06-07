1568746c355b3e16860d83d9fbc536bd0e9857b4
emkis <nicolasemkis@gmail.com>
2021-06-06 23:52:39 -0300

    feat: add movie detail route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,25 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import type { Component } from 'vue'
 
 import { environment } from '@/configs/environment'
 import { handlePageTitle } from './helpers'
 
 import { MovieCollection } from '@/pages/MovieCollection'
 
+export const MovieDetailLazy = (): Promise<Component> => {
+  return import(/* webpackChunkName: "MovieDetail" */ '@/pages/MovieDetail/MovieDetail.vue')
+}
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'MovieCollection',
     component: MovieCollection,
+  },
+  {
+    path: '/movie/:id',
+    name: 'MovieDetail',
+    component: MovieDetailLazy,
   },
   {
     path: '/:pathMatch(.*)',
