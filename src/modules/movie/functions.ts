fbcce40a4ab8e4e081e62c8c9e2ce20efcbc6beb
Nicolas Jardim <nicolasemkis@gmail.com>
2021-06-08 00:10:14 -0300

    refactor: change constant name

--- src/modules/movie/functions.ts
+++ src/modules/movie/functions.ts
@@ -1,9 +1,9 @@
-const BASE_MOVIE_SCORE = 55
+const AVERAGE_MOVIE_SCORE = 55
 
 export function isGoodReviewScore(score: number): boolean {
-  return score > BASE_MOVIE_SCORE
+  return score > AVERAGE_MOVIE_SCORE
 }
 
 export function isGoodMovieScore(score: number): boolean {
-  return score > BASE_MOVIE_SCORE
+  return score > AVERAGE_MOVIE_SCORE
 }
