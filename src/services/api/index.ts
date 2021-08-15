e2cf9589cb527b44c7190df9d8c35779079e576e
emkis <nicolasemkis@gmail.com>
2021-08-15 18:27:07 -0300

    feat: set retry and timeout to api service

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,10 +1,14 @@
 import axios from 'axios'
+import axiosRetry from 'axios-retry'
+
 import { environment } from '@/configs/environment'
 
 export function createApiService() {
   return axios.create({
     baseURL: environment.apiUrl,
+    timeout: 10000,
   })
 }
 
 export const ApiService = createApiService()
+axiosRetry(ApiService, { retries: 1 })
