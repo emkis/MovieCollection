598f262805a785da77b9e6c040accfafe01e5652
emkis <nicolasemkis@gmail.com>
2021-08-15 22:35:59 -0300

    feat: add cache adapter to api service

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,12 +1,18 @@
 import axios from 'axios'
 import axiosRetry from 'axios-retry'
+import { setupCache } from 'axios-cache-adapter'
 
 import { environment } from '@/configs/environment'
+
+const cache = setupCache({
+  maxAge: 15 * 60 * 3000, // 15 minutes
+})
 
 export function createApiService() {
   return axios.create({
     baseURL: environment.apiUrl,
     timeout: 10000,
+    adapter: cache.adapter,
   })
 }
 
