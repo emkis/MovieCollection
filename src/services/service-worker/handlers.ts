c6423cf4e7885836480b514b7c72d4cd48d8e7df
emkis <nicolasemkis@gmail.com>
2021-08-15 21:49:35 -0300

    feat: add movie review handler

--- src/services/service-worker/handlers.ts
+++ src/services/service-worker/handlers.ts
@@ -3,13 +3,34 @@ import { pick } from 'lodash-es'
 
 import { environment } from '@/configs/environment'
 import { movies } from './static-data'
+import type { Movie, MovieReview } from '@/modules/movie'
+
+const { apiUrl } = environment
+const defaultDelay = 1800
+
+function getMovieById(targetMovieId: string) {
+  const movie = movies.find((movie) => movie.id === targetMovieId)
+
+  if (!movie) {
+    throw new Error(`Movie id:"${targetMovieId}" not exists`)
+  }
+
+  return movie
+}
 
 export const handlers: RestHandler[] = [
-  rest.get(`${environment.apiUrl}/movies`, (request, response, context) => {
+  rest.get<never, Movie[]>(`${apiUrl}/movies`, (request, response, context) => {
     const parsedMovies = movies.map((movie) => {
       return pick(movie, 'id', 'name', 'category', 'score', 'year')
     })
 
-    return response(context.status(200), context.delay(2000), context.json(parsedMovies))
+    return response(context.status(200), context.delay(defaultDelay), context.json(parsedMovies))
+  }),
+
+  rest.get<never, MovieReview[]>(`${apiUrl}/movie-review/:id`, (request, response, context) => {
+    const { id: movieId } = request.params
+    const movie = getMovieById(movieId)
+
+    return response(context.status(200), context.delay(defaultDelay), context.json(movie.reviews))
   }),
 ]
