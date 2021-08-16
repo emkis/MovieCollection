27f872a146c78a57e461749b145b3cfa31da0354
emkis <nicolasemkis@gmail.com>
2021-08-15 21:50:01 -0300

    feat: make real fetchs in movie service

--- src/services/api/movie/index.ts
+++ src/services/api/movie/index.ts
@@ -1,48 +1,18 @@
-import { pick, omit } from 'lodash-es'
-
-import type { MovieCard, MovieDetail, MovieReview } from '@/modules/movie/types'
+import type { Movie, MovieDetail, MovieReview } from '@/modules/movie/types'
 import { ApiService } from '@/services/api'
-import { asyncDelay } from '@/utilities'
-import { movies } from './static-data'
 
-interface IMovieService {
-  fetchMovieCollection(delay?: number): Promise<MovieCard[]>
-  fetchMovieDetails(targetMovieId: string, delay?: number): Promise<MovieDetail>
-  fetchMovieReviews(targetMovieId: string, delay?: number): Promise<MovieReview[]>
-}
-
-function getMovieById(targetMovieId: string) {
-  const movie = movies.find((movie) => movie.id === targetMovieId)
-
-  if (!movie) {
-    throw new Error(`Movie id:"${targetMovieId}" not exists`)
-  }
-
-  return movie
-}
-
-function createMovieService(_httpClient = ApiService): IMovieService {
+function createMovieService(httpClient = ApiService) {
   return {
-    async fetchMovieCollection(delay = 1500) {
-      await asyncDelay(delay)
-
-      return movies.map((movie) => {
-        return pick(movie, 'id', 'name', 'category', 'score', 'year')
-      })
+    async fetchMovieCollection() {
+      return httpClient.get<Movie[]>('/movies')
     },
 
-    async fetchMovieReviews(targetMovieId: string, delay = 1500) {
-      await asyncDelay(delay)
-
-      const movie = getMovieById(targetMovieId)
-      return pick(movie, ['reviews']).reviews
+    async fetchMovieReviews(movieId: string) {
+      return httpClient.get<MovieReview[]>(`/movie-review/${movieId}`)
     },
 
-    async fetchMovieDetails(targetMovieId: string, delay = 1500) {
-      await asyncDelay(delay)
-
-      const movie = getMovieById(targetMovieId)
-      return omit(movie, ['reviews'])
+    async fetchMovieDetails(movieId: string) {
+      return httpClient.get<MovieDetail[]>(`/movie-detail/${movieId}`)
     },
   }
 }
