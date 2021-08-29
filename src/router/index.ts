46cc39ec8f83e4f80557578d94aa53c0c755d82e
emkis <nicolasemkis@gmail.com>
2021-08-29 10:58:50 -0300

    chore: add app base url

--- src/router/index.ts
+++ src/router/index.ts
@@ -29,7 +29,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(environment.appUrl),
+  history: createWebHistory(environment.baseUrl),
   routes,
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/app',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
