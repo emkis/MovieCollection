166df1534b01967eebfd021bfe953484d2622621
emkis <nicolasemkis@gmail.com>
2021-08-28 22:19:33 -0300

    chore: change configurations to work on vite

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,5 +1,5 @@
 export const environment = Object.freeze({
-  appTitle: process.env.VUE_APP_TITLE || '[dev] Movie Collection',
-  appUrl: process.env.VUE_APP_BASE_URL || '/',
-  apiUrl: process.env.VUE_APP_API_URL || 'http://localhost:3333',
+  appTitle: import.meta.env.VUE_APP_TITLE || '[dev] Movie Collection',
+  appUrl: import.meta.env.VUE_APP_BASE_URL || '/',
+  apiUrl: import.meta.env.VUE_APP_API_URL || 'http://localhost:3333',
 })
