5270db4d73efc56737e3cd44d959190371b0ad80
emkis <nicolasemkis@gmail.com>
2021-09-12 16:06:37 -0300

    chore(vite): add build sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
