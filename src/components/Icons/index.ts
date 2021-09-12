01ec767d83ebd2c073761b428b17572cb0d80105
emkis <nicolasemkis@gmail.com>
2021-09-12 01:51:38 -0300

    feat: add popcorn icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as IconArrowLeft } from './IconArrowLeft.vue'
 export { default as IconHeart } from './IconHeart.vue'
 export { default as IconHalfHeart } from './IconHalfHeart.vue'
+export { default as IconPopcorn } from './IconPopcorn.vue'
