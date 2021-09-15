95d813d689f677f3b1336823fdb0fd4431de6f82
emkis <nicolasemkis@gmail.com>
2021-09-15 02:11:48 -0300

    refactor: remove duplicated function

--- src/modules/movie/score.ts
+++ src/modules/movie/score.ts
@@ -1,10 +1,6 @@
 const AVERAGE_MOVIE_SCORE = 55
 const MAX_HEART_SCORE = 5
 
-export function isGoodReviewScore(score: number): boolean {
-  return score > AVERAGE_MOVIE_SCORE
-}
-
 export function isGoodMovieScore(score: number): boolean {
   return score > AVERAGE_MOVIE_SCORE
 }
