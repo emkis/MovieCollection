21859f3cc6e3023250212655824bde78e6a3e939
emkis <nicolasemkis@gmail.com>
2021-09-15 01:12:48 -0300

    refactor: remove unused script block

--- src/components/Container/Container.vue
+++ src/components/Container/Container.vue
@@ -4,14 +4,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'Container',
-})
-</script>
-
 <style lang="scss" scoped>
 .Container {
   padding: rem(72px 15px);
