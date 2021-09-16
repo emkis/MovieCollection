3ae68df45fd65afb72f82f4c6bdb87508c659e33
emkis <nicolasemkis@gmail.com>
2021-09-16 00:20:47 -0300

    refactor: change page title hook to use ref

--- src/hooks/page-title/index.ts
+++ src/hooks/page-title/index.ts
@@ -1,21 +1,32 @@
+import { ref, watchEffect, readonly } from 'vue'
 import { environment } from '@/configs/environment'
 
-interface IOptions {
-  appendTitle?: boolean
+type SetTitleOptions = {
+  appendAppName?: boolean
 }
 
 export function usePageTitle() {
   const { appTitle } = environment
+  const currentTitle = document.title
+  const title = ref(currentTitle)
 
-  function setTitle(pageName: string, { appendTitle = true }: IOptions = {}) {
-    if (appendTitle) {
-      document.title = `${pageName} | ${appTitle}`
+  watchEffect(() => setAppTitle(title.value))
+
+  function setAppTitle(title: string) {
+    document.title = title
+  }
+
+  function resetTitle() {
+    setTitle(appTitle, { appendAppName: false })
+  }
+
+  function setTitle(pageName: string, { appendAppName = true }: SetTitleOptions = {}) {
+    if (appendAppName) {
+      title.value = `${pageName} on ${appTitle}`
     } else {
-      document.title = pageName
+      title.value = pageName
     }
   }
 
-  const resetTitle = () => setTitle(appTitle, { appendTitle: false })
-
-  return { setTitle, resetTitle }
+  return { title: readonly(title), setTitle, resetTitle }
 }
