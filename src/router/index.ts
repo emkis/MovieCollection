a4aaee9c458f2a26136119c3de1e52dc5811aabe
emkis <nicolasemkis@gmail.com>
2021-09-16 00:47:13 -0300

    refactor: remove routes declaration and types

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,7 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { environment } from '@/configs/environment'
 import { setRouteTitle } from '@/router/route-title'
-
-import { MovieCollection } from '@/pages/MovieCollection'
-import { MovieDetailLazy } from '@/router/lazy-routes'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'MovieCollection',
-    component: MovieCollection,
-  },
-  {
-    path: '/movie/:slug',
-    name: 'MovieDetail',
-    component: MovieDetailLazy,
-  },
-  {
-    path: '/:pathMatch(.*)',
-    name: 'NotFound',
-    redirect: '/',
-  },
-]
+import { routes } from '@/router/routes'
 
 export const router = createRouter({
   history: createWebHistory(environment.baseUrl),
