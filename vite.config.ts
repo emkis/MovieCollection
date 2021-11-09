597cb3c01b83c41df2ced1c985f7df1bd9ca2dd6
emkis <nicolasemkis@gmail.com>
2021-11-08 23:14:21 -0300

    chore(vite): change global tokens importh path

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/constants/index.scss";`,
+        additionalData: `@import "@/styles/tokens/index.scss";`,
       },
     },
   },
