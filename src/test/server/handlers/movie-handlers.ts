89519caa2dd97bf3d49c54f16a2eddc3854d4477
emkis <nicolasemkis@gmail.com>
2021-11-13 20:54:18 -0300

    refactor: get movies from movie database

--- src/test/server/handlers/movie-handlers.ts
+++ src/test/server/handlers/movie-handlers.ts
@@ -1,25 +1,16 @@
 import { rest, RestHandler } from 'msw'
 import { omit, pick } from 'lodash-es'
-
 import { environment } from '@/configs/environment'
-import { movies } from '@/test/data/movies'
+import { movieDatabase } from '@/test/data/movies-database'
 import type { Movie, MovieDetail, MovieReview } from '@/services/api/movie'
 
 const { apiUrl } = environment
 const defaultDelay = 1800
 
-function getMovieBySlug(movieSlug: string) {
-  const movie = movies.find((movie) => movie.slug === movieSlug)
-
-  if (!movie) {
-    throw new Error(`Movie slug:"${movieSlug}" not found`)
-  }
-
-  return movie
-}
-
 export const movieHandlers: RestHandler[] = [
   rest.get<never, Movie[]>(`${apiUrl}/movies`, (_, response, context) => {
+    const movies = movieDatabase.getAll()
+
     const parsedMovies = movies.map((movie) => {
       return pick(movie, 'id', 'slug', 'name', 'category', 'score', 'year')
     })
@@ -28,13 +19,13 @@ export const movieHandlers: RestHandler[] = [
   }),
 
   rest.get<never, MovieReview[]>(`${apiUrl}/movie-review/:slug`, (request, response, context) => {
-    const movie = getMovieBySlug(request.params.slug)
+    const movie = movieDatabase.getBySlug(request.params.slug)
 
     return response(context.status(200), context.delay(defaultDelay), context.json(movie.reviews))
   }),
 
   rest.get<never, MovieDetail>(`${apiUrl}/movie-detail/:slug`, (request, response, context) => {
-    const movie = getMovieBySlug(request.params.slug)
+    const movie = movieDatabase.getBySlug(request.params.slug)
     const movieDetails = omit(movie, ['reviews'])
 
     return response(context.status(200), context.delay(defaultDelay), context.json(movieDetails))
