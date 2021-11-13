4c65c79511378ed87f74b10b6a8a630e93a87fef
emkis <nicolasemkis@gmail.com>
2021-11-13 17:45:26 -0300

    docs: add descriptions to env variables

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,7 +1,19 @@
 export const environment = Object.freeze({
+  /**
+   * The base URL the app is being served from, default value is "/".
+   * If the value were "/app" the URL would be: https://loremipsum.com/app/
+   */
   baseUrl: import.meta.env.BASE_URL,
+
+  /** Default title of this app, will be set on document.title */
   appTitle: import.meta.env.VITE_APP_TITLE,
+
+  /** The base URL of the API */
   apiUrl: import.meta.env.VITE_APP_API_URL,
+
+  /** Whether the app is running in development */
   isDevelopment: import.meta.env.DEV,
+
+  /** Whether the app is running in production */
   isProduction: import.meta.env.PROD,
 })
