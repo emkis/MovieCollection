ddfe6aef83f6ea1f43b8801a6acfe956c8eb7d2d
emkis <nicolasemkis@gmail.com>
2021-11-13 17:29:19 -0300

    feat: add mode environment variables

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -2,4 +2,6 @@ export const environment = Object.freeze({
   baseUrl: import.meta.env.BASE_URL,
   appTitle: import.meta.env.VITE_APP_TITLE,
   apiUrl: import.meta.env.VITE_APP_API_URL,
+  isDevelopment: import.meta.env.DEV,
+  isProduction: import.meta.env.PROD,
 })
