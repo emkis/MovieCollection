d2dcece45bdd9e0556b3e858b8cb33e4e33b2727
emkis <nicolasemkis@gmail.com>
2021-11-13 21:59:45 -0300

    feat: add not found response for movies

--- src/test/server/handlers/movie-handlers.ts
+++ src/test/server/handlers/movie-handlers.ts
@@ -1,4 +1,4 @@
-import { rest, RestHandler } from 'msw'
+import { rest, RestHandler, response, context } from 'msw'
 import { omit, pick } from 'lodash-es'
 import { environment } from '@/configs/environment'
 import { movieDatabase } from '@/test/data/movies-database'
@@ -6,6 +6,14 @@ import type { Movie, MovieDetail, MovieReview } from '@/services/api/movie'
 
 const { apiUrl } = environment
 const defaultDelay = 1800
+
+function notFoundResponse() {
+  return response(
+    context.status(404),
+    context.delay(defaultDelay),
+    context.json({ message: 'Movie not found' })
+  )
+}
 
 export const movieHandlers: RestHandler[] = [
   rest.get<never, Movie[]>(`${apiUrl}/movies`, (_, response, context) => {
@@ -20,14 +28,16 @@ export const movieHandlers: RestHandler[] = [
 
   rest.get<never, MovieReview[]>(`${apiUrl}/movie-review/:slug`, (request, response, context) => {
     const movie = movieDatabase.getBySlug(request.params.slug)
+    if (!movie) return notFoundResponse()
 
     return response(context.status(200), context.delay(defaultDelay), context.json(movie.reviews))
   }),
 
   rest.get<never, MovieDetail>(`${apiUrl}/movie-detail/:slug`, (request, response, context) => {
     const movie = movieDatabase.getBySlug(request.params.slug)
-    const movieDetails = omit(movie, ['reviews'])
+    if (!movie) return notFoundResponse()
 
+    const movieDetails = omit(movie, ['reviews'])
     return response(context.status(200), context.delay(defaultDelay), context.json(movieDetails))
   }),
 ]
