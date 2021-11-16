9d973afc81cec2744c3defa61412a16ec8c9c120
emkis <nicolasemkis@gmail.com>
2021-11-16 00:34:08 -0300

    refactor: remove unnecessary cache configuration

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,17 +1,11 @@
 import axios from 'axios'
 import axiosRetry from 'axios-retry'
-import { setupCache } from 'axios-cache-adapter'
 import { environment } from '@/configs/environment'
-
-const cache = setupCache({
-  maxAge: 10 * 60 * 1000, // 10 minutes
-})
 
 export function createApiService(baseURL: string) {
   return axios.create({
     baseURL,
     timeout: 10000,
-    adapter: cache.adapter,
   })
 }
 
