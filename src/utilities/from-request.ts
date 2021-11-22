0cd56468de854116d87b554bd3a1c2771fa2a0c0
emkis <nicolasemkis@gmail.com>
2021-11-21 23:59:19 -0300

    refactor: change utility file name

--- src/utilities/from-request.ts
+++ src/utilities/from-request.ts
@@ -1,6 +1,6 @@
 import type { AxiosResponse } from 'axios'
 
-export async function getResponseData<T>(fetcher: () => Promise<AxiosResponse<T>>) {
+export async function getDataFromRequest<T>(fetcher: () => Promise<AxiosResponse<T>>) {
   const response = await fetcher()
   return response.data
 }
