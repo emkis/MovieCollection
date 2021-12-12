2de04462fc859ad88fe4df35aaf3b71bc9785d62
emkis <nicolasemkis@gmail.com>
2021-12-12 19:06:38 -0300

    refactor: add create key function

--- src/pages/Movie/queries/movie-reviews-query.ts
+++ src/pages/Movie/queries/movie-reviews-query.ts
@@ -3,12 +3,16 @@ import { MovieService, MovieReview } from '@/services/api/movie'
 import { getDataFromRequest } from '@/utilities/from-request'
 import { movieKeys } from './movie-query-keys'
 
+export function createQueryKey(movieSlug: string) {
+  return movieKeys.reviewWithSlug(movieSlug)
+}
+
 export function useMovieReviewsQuery(movieSlug: string) {
   const fetchMovieReviews = () => {
     return getDataFromRequest(() => MovieService.fetchMovieReviews(movieSlug))
   }
 
-  return useQuery<MovieReview[]>(movieKeys.reviewWithSlug(movieSlug), fetchMovieReviews, {
+  return useQuery<MovieReview[]>(createQueryKey(movieSlug), fetchMovieReviews, {
     staleTime: 2000 * 60, // 2 minutes
   })
 }
