14d9dabcc4eb982883d78aa7e681903c67135400
emkis <nicolasemkis@gmail.com>
2021-12-12 18:26:27 -0300

    feat: add centralized movie keys

---
+++ src/pages/Movie/queries/movie-query-keys.ts
@@ -0,0 +1,9 @@
+export const movieKeys = {
+  all: ['movie'] as const,
+
+  allDetails: () => [...movieKeys.all, 'detail'] as const,
+  detailWithSlug: (slug: string) => [...movieKeys.allDetails(), slug] as const,
+
+  allReviews: () => [movieKeys.all, 'review'] as const,
+  reviewWithSlug: (slug: string) => [...movieKeys.allReviews(), slug] as const,
+}
