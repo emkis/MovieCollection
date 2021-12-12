37a4c5009ca9f8b9206239f286735e945dce7e7b
emkis <nicolasemkis@gmail.com>
2021-12-12 18:27:25 -0300

    refactor: get query keys from file

--- src/pages/Movie/queries/movie-details-query.ts
+++ src/pages/Movie/queries/movie-details-query.ts
@@ -1,18 +1,14 @@
 import { useQuery } from 'vue-query'
 import { MovieService } from '@/services/api/movie'
 import { getDataFromRequest } from '@/utilities/from-request'
-
-export const movieDetailsKeys = {
-  all: ['movie', 'detail'] as const,
-  specific: (slug: string) => [...movieDetailsKeys.all, slug] as const,
-}
+import { movieKeys } from './movie-query-keys'
 
 export function useMovieDetailsQuery(movieSlug: string) {
   const fetchMovieDetails = () => {
     return getDataFromRequest(() => MovieService.fetchMovieDetails(movieSlug))
   }
 
-  return useQuery(movieDetailsKeys.specific(movieSlug), fetchMovieDetails, {
+  return useQuery(movieKeys.detailWithSlug(movieSlug), fetchMovieDetails, {
     staleTime: Infinity,
   })
 }
--- src/pages/Movie/queries/movie-reviews-query.ts
+++ src/pages/Movie/queries/movie-reviews-query.ts
@@ -1,18 +1,14 @@
 import { useQuery } from 'vue-query'
 import { MovieService, MovieReview } from '@/services/api/movie'
 import { getDataFromRequest } from '@/utilities/from-request'
-
-export const movieReviewsKeys = {
-  all: ['movie', 'review'] as const,
-  specific: (slug: string) => [...movieReviewsKeys.all, slug] as const,
-}
+import { movieKeys } from './movie-query-keys'
 
 export function useMovieReviewsQuery(movieSlug: string) {
   const fetchMovieReviews = () => {
     return getDataFromRequest(() => MovieService.fetchMovieReviews(movieSlug))
   }
 
-  return useQuery<MovieReview[]>(movieReviewsKeys.specific(movieSlug), fetchMovieReviews, {
+  return useQuery<MovieReview[]>(movieKeys.reviewWithSlug(movieSlug), fetchMovieReviews, {
     staleTime: 2000 * 60, // 2 minutes
   })
 }
