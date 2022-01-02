ef669291131bd1a93c2e032c003e05dcc0a85d4c
emkis <nicolasemkis@gmail.com>
2022-01-01 23:40:03 -0300

    refactor: change component name

---
+++ src/components/SafeEdgesContainer/SafeEdgesContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="SafeEdgesContainer">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.SafeEdgesContainer {
+  padding: 0 $spacing-xxxs;
+
+  @media (min-width: $breakpoint-m) {
+    padding: 0 $spacing-xxs;
+  }
+}
+</style>
---
+++ src/components/SafeEdgesContainer/index.ts
@@ -0,0 +1,1 @@
+export { default as SafeEdgesContainer } from './SafeEdgesContainer.vue'
