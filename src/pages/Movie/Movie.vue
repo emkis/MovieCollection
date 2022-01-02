654a76103e3d3aaa4b84f8cacfc68347c85820d6
emkis <nicolasemkis@gmail.com>
2022-01-02 16:11:39 -0300

    feat: add new page styles

--- src/pages/Movie/Movie.vue
+++ src/pages/Movie/Movie.vue
@@ -1,28 +1,40 @@
 <template>
-  <div v-if="movieDetailsQuery.isSuccess.value" class="Movie">
-    <div class="Movie__poster">
-      <img alt="Movie Poster" :src="movie!.poster" />
+  <div v-if="movieDetailsQuery.isSuccess.value">
+    <div class="movie-poster">
+      <img :src="movie!.poster" alt="" aria-hidden="true" />
     </div>
 
-    <main class="Movie__presentation">
-      <Heading level="2">{{ movie!.name }}</Heading>
-      <MovieInfo
-        class="Movie__about"
-        :year="movie!.year"
-        :duration="movie!.duration"
-        :category="movie!.category"
-      />
-      <Text class="Movie__rating">
-        <MovieHeartScore :score="movie!.score" />
-        <Text as="strong">{{ movie!.score }}%</Text>
-      </Text>
-      <Text class="Movie__description">{{ movie!.description }}</Text>
-    </main>
+    <SafeEdgesContainer>
+      <main class="content-container">
+        <section>
+          <Heading level="1">{{ movie!.name }}</Heading>
+          <div class="movie-specification">
+            <MovieInfo
+              :year="movie!.year"
+              :duration="movie!.duration"
+              :category="movie!.category"
+            />
+            <Text class="movie-score">
+              <MovieHeartScore :score="movie!.score" />
+              <Text as="span" :color="ThemeColors.white">{{ movie!.score }}%</Text>
+            </Text>
+          </div>
+        </section>
 
-    <Suspense>
-      <MovieReviews :movieSlug="movieSlug" />
-      <template #fallback> Loading... </template>
-    </Suspense>
+        <section>
+          <Heading level="2">About</Heading>
+          <Text>{{ movie!.description }}</Text>
+        </section>
+
+        <section>
+          <Heading level="2">Critic Reviews</Heading>
+          <Suspense>
+            <MovieReviews :movieSlug="movieSlug" />
+            <template #fallback>Loading...</template>
+          </Suspense>
+        </section>
+      </main>
+    </SafeEdgesContainer>
   </div>
 </template>
 
@@ -31,7 +43,9 @@ import { watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useMovieDetailsQuery } from './queries/movie-details-query'
 import { usePageTitle } from '@/hooks/use-page-title'
+import { ThemeColors } from '@/services/theme'
 
+import { SafeEdgesContainer } from '@/components/SafeEdgesContainer'
 import { Heading } from '@/components/Heading'
 import { Text } from '@/components/Text'
 import { MovieInfo } from '@/components/MovieInfo'
@@ -56,67 +70,63 @@ watch(movie, setMovieNameOnPage, { immediate: true })
 </script>
 
 <style lang="scss" scoped>
-$poster-height: 400px;
+.movie-poster {
+  position: relative;
+  height: 519px;
 
-.Movie {
-  max-width: 768px;
-  margin: 0 auto;
-  padding: 0 24px 72px;
+  @media (min-width: $breakpoint-m) {
+    height: 623px;
+  }
 
-  &__poster {
+  > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &::before {
+    content: '';
     position: absolute;
+    width: 100%;
+    height: 100%;
     top: 0;
     left: 0;
-    height: $poster-height;
-    width: 100%;
-
-    > img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-
-    &::before {
-      content: '';
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      top: 0;
-      left: 0;
-      background: linear-gradient(
-        180deg,
-        rgba(196, 196, 196, 0) 0%,
-        rgba(78, 79, 83, 0.688174) 59.56%,
-        $color-shark 91.25%
-      );
-    }
+    background: linear-gradient(
+      180deg,
+      rgba(24, 26, 32, 0.37) 28.04%,
+      rgba(24, 26, 32, 0.94) 74.27%,
+      rgba(24, 26, 32, 0.97) 79.98%,
+      rgba(24, 26, 32, 0.99) 85.69%,
+      #181a20 91.25%
+    );
   }
+}
 
-  &__presentation {
-    position: relative;
-    padding-top: calc(#{$poster-height} - 110px);
-    text-align: center;
+.content-container {
+  position: relative;
+  max-width: 1200px;
+  margin: -280px auto 0;
+  padding-bottom: $spacing-xxxl;
+  text-align: left;
+}
+
+section {
+  display: grid;
+  gap: $spacing-xxs;
+
+  & + section {
+    margin-top: $spacing-xl;
   }
+}
 
-  &__about {
-    display: block;
-    margin: 24px 0 14px;
-  }
+.movie-specification {
+  display: grid;
+  gap: $spacing-xxxs;
+}
 
-  &__rating {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    > strong {
-      margin-left: 6px;
-      color: $color-white;
-    }
-  }
-
-  &__description {
-    text-align: left;
-    margin: 72px 0 120px;
-  }
+.movie-score {
+  display: flex;
+  align-items: center;
+  gap: $spacing-nano;
 }
 </style>
